e59a3a63d8b8b5f92c02520c8eed84e6193d5c38
arekiu <alex.isaguilera@hotmail.com>
2024-08-09 11:24:49 +0200

    do structures

--- src/philosophers.h
+++ src/philosophers.h
@@ -18,6 +18,38 @@
 # include <limits.h>
 # include <unistd.h>
 
+typedef struct s_simulation
+{
+    int     number_philos;
+    int     time_to_die;
+    int     time_to_eat;
+    int     time_to_sleep;
+    int     max_meals;
+    long    start_time;
+    int     end_simulation;
+    t_fork  *forks;
+    t_philosopher *philosophers;
+}       t_simulation;
+
+typedef struct s_fork
+{
+    pthread_mutex_t fork;
+    int             fork_id;
+}                   t_fork;
+
+
+typedef struct s_philosopher
+{
+    int         id;
+    int         meals;
+    int         max_meals;
+    long        last_meal;
+    t_fork      *left_fork;
+    t_fork      *right_fork;
+    pthread_t   thread_id;
+    t_simulation    *simulation;
+}               t_philosopher;
+
 int is_valid(int argc, char *argv[]);
 
 #endif
